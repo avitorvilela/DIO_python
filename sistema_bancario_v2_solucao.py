d4b33439b95636c52a6f754986fee59c4af10b67
Antônio <avitorvilela@gmail.com>
2023-08-18 10:34:50 -0300

    Sistema bancário com funções.

---
+++ sistema_bancario_v2_solucao.py
@@ -0,0 +1,30 @@
+import textwrap
+
+def menu():
+    ...
+
+def depositar(saldo, valor, extrato, /):
+    ...
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    ...
+
+def exibir_extrato(saldo, /, *, extrato):
+    ...
+
+def criar_usuario(usuarios):
+    ...
+
+def filtrar_usuario(cpf, usuarios):
+    ...
+
+def criar_conta(agencia, numero_conta, usuarios):
+    ...
+
+def listar_contas(contas):
+    ...
+
+def main():
+    ...
+
+main()
